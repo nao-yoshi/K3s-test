5bf47922092e238e0715e12329353edaf2ff536b
nao-yoshi <61136455+nao-yoshi@users.noreply.github.com>
2020-04-22 21:20:07 +0900

    v0.1

---
+++ scripts/bootstrap.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+## Set timezone
+sudo cp /usr/share/zoneinfo/Japan /etc/localtime
+echo "========================================="
+echo "set timezone as JST"
+echo "========================================="
+
+## Disable firewalld
+sudo systemctl stop firewalld
+sudo systemctl disable firewalld
+
+## Install git
+sudo yum -y install git
+echo "========================================="
+echo `git --version`
+echo "========================================="
